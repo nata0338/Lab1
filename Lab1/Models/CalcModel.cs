be9dcbbe28041e504880ea3b1df41733f5477b42
nata0338 <72197071+nata0338@users.noreply.github.com>
2021-04-27 10:49:50 +0700

    Add files via upload

---
+++ Lab1/Models/CalcModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1.Models {
+    public class CalcModel {
+        public readonly int X;
+        public readonly int Y;
+        public readonly string Sum;
+        public readonly string Sub;
+        public readonly string Mult;
+        public readonly string Div;
+        public CalcModel(int x, int y, string sum, string sub, string mult, string div) {
+            X = x;
+            Y = y;
+            Sum = sum;
+            Sub = sub;
+            Mult = mult;
+            Div = div;
+        }
+    }
+}
